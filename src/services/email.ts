f97411396b34fafb1a1a14af533626e4dfd4f8d6
Peter Szollar <szollarp@gmail.com>
2019-04-23 08:48:37 +0200

    catch email seding errors

--- src/services/email.ts
+++ src/services/email.ts
@@ -1,14 +1,17 @@
 import config from 'config';
 import axios from 'axios';
 import { Context } from '../context';
+import { RequestError } from '../utils/errors';
 
 const send = async (context: Context, to: string, message: string, subject: string) => {
-  const { from, headerInfo, endpoint } = config.get('email');
-  const mailOptions = { from, to, subject, message };
-  
-  await axios.post(endpoint, mailOptions, headerInfo);
-  context.logger.info(`E-mail message sent to ${to}`);
-  context.logger.info(message);
+  try {
+    const { from, headerInfo, endpoint } = config.get('email');
+    const mailOptions = { from, to, subject, message };
+    
+    await axios.post(endpoint, mailOptions, headerInfo);
+    context.logger.info(`E-mail message sent to ${to}`);
+    context.logger.info(message);
+  } catch(error) { throw new RequestError(error.toString()) }
 }
 
 export const sendNewContractCreated = async (context: Context, to: string, message: string) => {
